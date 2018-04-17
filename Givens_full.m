198309f510b91bf28c5491306fb650197b027371
Sun, Daoxun <sdxmonkey@gatech.edu>
2018-04-16 21:33:15 -0400

    modify Givens_full
    Add the option to utilize the bandwith information

--- Givens_full.m
+++ Givens_full.m
@@ -1,16 +1,19 @@
-function [x,A] = Givens_full(A,b)
+function [x,A] = Givens_full(A,b,bw)
 %GIVENS_QR solve sparse matrix Ax = b, A & b will be in full matrix format;
 % Here A is full ranked
 %   1. Apply QR decomposition on A: A = QR;
 %   2. solve the triadiagonal matrix: Q'Ax = Q'b
 %input:
 %   A,b for system Ax = b
+%   bw: bw <= 0: give no bandwith information; bw>0: assume certain bandwith,
+%   all entries beyond the bandwith will be ingnored.
 %output:
 %   x: solution of Ax = b;
     [m,n] = size(A);
     % QRD
+ if bw <= 0
     for j = 1:n
-        i = m;
+        i = find(A(:,j),1,'last');
         while  i > j
             if (A(i,j)~=0)
                 k = i-1;
@@ -18,7 +21,7 @@ function [x,A] = Givens_full(A,b)
                     k = k-1;
                 end
                 [c,s] = givens(A(k,j),A(i,j));
-                A([k,i],j:n) = [c,s;-s,c]'*A([k,i],j:n);
+                A([k,i],(A(k,:)~=0 |A(i,:)~=0)) = [c,s;-s,c]'*A([k,i],(A(k,:)~=0 |A(i,:)~=0));
                 b([k,i]) = [c,s;-s,c]'*b([k,i]);
                 i = k;
             else
@@ -26,18 +29,38 @@ function [x,A] = Givens_full(A,b)
             end
         end
     end
-    if A(n,n) == 0
-        disp('ERROR: rank(A) should equal to n')
-        x = NaN;
-    else
-        % solve x;
-        b(n) = b(n)/(A(n,n));
-        for i = n-1:-1:1
-            b(1:i) = b(1:i) - A(1:i,i+1)*b(i+1);
-            b(i) = b(i)/A(i,i);
+ else
+     for j = 1:n
+        mm = min(m,j+bw);
+        i = mm;
+        while  i > j
+            if (A(i,j)~=0)
+                k = i-1;
+                while (k >= j && A(k,j) == 0)
+                    k = k-1;
+                end
+                [c,s] = givens(A(k,j),A(i,j));
+                A([k,i],j:min(n,mm+bw)) = [c,s;-s,c]'*A([k,i],j:min(n,mm+bw));
+                b([k,i]) = [c,s;-s,c]'*b([k,i]);
+                i = k;
+            else
+                i = i-1;
+            end
         end
-        x = b(1:n);
+     end
+end
+if A(n,n) == 0
+    disp('ERROR: rank(A) should equal to n')
+    x = NaN;
+else
+    % solve x;
+    b(n) = b(n)/(A(n,n));
+    for i = n-1:-1:1
+        b(1:i) = b(1:i) - A(1:i,i+1)*b(i+1);
+        b(i) = b(i)/A(i,i);
     end
+    x = b(1:n);
+end
 end
 
 
--- test_Givens.m
+++ test_Givens.m
@@ -1,19 +1,20 @@
 %% test the givens solvers for the problem
 clear all;
-nodes = 100; N=2*nodes;
+nodes = 500; N=2*nodes;
 [K,F,ymax,id] = mkbeamproblem(nodes);
+disp('Applying matlab inverse method ...')
+tic;xMatlab=K\F;tM = toc
+err = ymax - xMatlab(id);
 disp('Applying Givens_full method ...')
-tic;[xf,Af] = Givens_full(K,F);toc
+tic;[xf,Af] = Givens_full(K,F,3);tf = toc
+norm(xMatlab-xf) % check the difference Givens_full and in-built solver
 errG_f = ymax - xf(id);
+errG_f2 = ymax - xf2(id);
 K_COO = full2sparse(K,'COO');
 F_COO = full2sparse(F,'COO');
 disp('Applying Givens_COO method ...')
-tic;[x_COO,A_COO] = Givens_COO(K_COO,F_COO);toc
+tic;[x_COO,A_COO] = Givens_COO(K_COO,F_COO);tC = toc
 xC = sparse2full(x_COO,'COO');
 AC = sparse2full(A_COO,'COO');
 errG_C = ymax - xC(id);
-disp('Applying matlab inverse method ...')
-tic;xMatlab=K\F;toc
-err = ymax - xMatlab(id);
-norm(xMatlab-xf) % check the difference Givens_full and in-built solver
 norm(xMatlab-xC) % check the difference Givens_COO and in-built solver
