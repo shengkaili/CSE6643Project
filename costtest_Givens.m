580fe4c73dd3ab6ad96073db9ddf569c63e35ea9
Sun, Daoxun <sdxmonkey@gatech.edu>
2018-04-16 22:21:21 -0400

    Another modification

--- costtest_Givens.m
+++ costtest_Givens.m
@@ -3,9 +3,11 @@ nodeArray=10:10:300;
 tM = nan(length(nodeArray),1);
 tGf = nan(length(nodeArray),1);
 tGf_bw = nan(length(nodeArray),1);
+tGC = nan(length(nodeArray),1);
 errM = nan(length(nodeArray),1);
 errG_full = nan(length(nodeArray),1);
 errG_full_bw = nan(length(nodeArray),1);
+errG_COO = nan(length(nodeArray),1);
 for i=1:length(nodeArray)
     nodes=nodeArray(i);
     [K,F,ymax,id] = mkbeamproblem(nodes);
@@ -16,13 +18,13 @@ for i=1:length(nodeArray)
     end
     tic;[xf,Af] = Givens_full(K,F,0);tGf(i) = toc;
     errG_full(i) = ymax-xf(id);
-%     K_COO = full2sparse(K,'COO');
-%     F_COO = full2sparse(F,'COO');
-%     tic;[x_COO,A_COO] = Givens_COO(K_COO,F_COO);tGC(i) = toc;
-%     xC = sparse2full(x_COO,'COO');
-%     errG_COO(i) = ymax-xC(id);
     tic;[xf_bw,Af_bw] = Givens_full(K,F,3);tGf_bw(i) = toc;
     errG_full_bw(i) = ymax-xf_bw(id);
+    K_COO = full2sparse(K,'COO');
+    F_COO = full2sparse(F,'COO');
+    tic;[x_COO,A_COO] = Givens_COO(K_COO,F_COO);tGC(i) = toc;
+    xC = sparse2full(x_COO,'COO');
+    errG_COO(i) = ymax-xC(id);
 end
 FS = 18;
 figure(1)
@@ -65,11 +67,11 @@ subplot(2,2,4)
 loglog(nodeArray,tM/t0,'o-');
 hold on
 loglog(nodeArray,tGf/t0,'o-');
-% plot(nodeArray,log(tGC/t0),'o-');
 loglog(nodeArray,tGf_bw/t0,'o-');
+loglog(nodeArray,tGC/t0,'o-');
 hold off
 xlabel('nodes'); ylabel('relative time cost');
-legend('Matlab in-built','Givens (no bandwith information)','Givens (known bandwith)','location','NorthWest');
+legend('Matlab in-built','Givens (no bandwith information)','Givens (known bandwith)','Givens for COO matrix','location','SouthEast');
 grid on
 xlim([min(nodeArray) max(nodeArray)])
 set(gca,'FontSize',FS)
